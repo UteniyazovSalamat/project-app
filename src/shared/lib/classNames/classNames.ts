34db67c3d77982b54e06a1a3f76ef8f4a702890b
UteniyazovSalamat <sss19855891@gmail.com>
2025-02-25 19:58:30 -0600

    feat: add eslint, fix all files

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -6,6 +6,7 @@ export function classNames(cls: string, mods: {}, additional: string[] = []): st
         ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
-            .map(([className]) => className)
-    ].join(' ');
+            .map(([className]) => className),
+    ]
+        .join(' ');
 }
